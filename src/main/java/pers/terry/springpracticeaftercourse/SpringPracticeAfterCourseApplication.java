1b5a6bffc6d72c9f2618d3b86abe93270472ece2
Terry Tan <fuling19.me@gmail.com>
2024-03-27 20:59:44 +0800

    feat: init project

---
+++ src/main/java/pers/terry/springpracticeaftercourse/SpringPracticeAfterCourseApplication.java
@@ -0,0 +1,13 @@
+package pers.terry.springpracticeaftercourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPracticeAfterCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPracticeAfterCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pers/terry/springpracticeaftercourse/SpringPracticeAfterCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.terry.springpracticeaftercourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticeAfterCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
